51fdbf01f447a79fce00a07a4e146944a05e6d68
Guilherme P Silva <guilpas@gmail.com>
2023-01-23 18:36:47 -0300

    Para fixar 2

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script2.js
@@ -0,0 +1,8 @@
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log("A area do retangulo é "+area);
+
+const perimeter = base*2+heigth*2;
+console.log("O perimetro do retangulo é "+perimeter);
