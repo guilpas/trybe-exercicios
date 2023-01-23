6eb2580b580a2067d9194a5c81511053502bd3ab
Guilherme P Silva <guilpas@gmail.com>
2023-01-23 18:29:42 -0300

    Para fixar 1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts1.js
@@ -0,0 +1,13 @@
+const myName = "Guilherme";
+const birthCity = "Uberlândia";
+let birthYear = 1990;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Minas"; //nao podemos alterar constante
+console.log(birthCity);
