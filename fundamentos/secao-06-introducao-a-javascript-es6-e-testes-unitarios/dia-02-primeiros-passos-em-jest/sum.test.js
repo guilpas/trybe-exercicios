f808ba012bbc36897d886c033f42afa1daa40535
Guilherme P Silva <guilpas@gmail.com>
2023-02-24 17:35:30 -0300

    exercicios jest, commit inicial

---
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,16 @@
+const {sum,myRemove} = require("./script");
+
+test('sums two values', () => {
+  expect(sum(2, 3)).toEqual(5);
+});
+
+describe('testa função myRemove', () => {
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;
+
+test('sums two values', () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+
+
+
+});
