765cf74828793af598a33bd5636a5798ffdad41c
Guilherme P Silva <guilpas@gmail.com>
2023-03-08 01:55:01 -0300

    Exercicios 2 a 6

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/exercise1.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/exercise1.js
@@ -1,6 +1,7 @@
 // Exercício 1
 // a função rectangleArea de forma que seja impressa a área dos 3 retângulos (rectangle1, rectangle2 e rectangle3). O código deve retornar em sequência 2, 15 e 54.
 
+console.log("Exercicio 1");
 const rectangleArea = (width, height) => width * height;
 
 const rectangle1 = [1, 2];
@@ -11,3 +12,131 @@ const rectangles = [rectangle1, rectangle2, rectangle3];
 rectangles.forEach((rectangle) => {
   console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
 });
+
+// Exercício 2
+// Crie uma função sum que, dado um número ilimitado de parâmetros, retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6), o número 15 deve ser retornado.
+console.log("Exercicio 2");
+
+const sum = (...nums) => nums.reduce( (total,num) => total+num ,0);
+
+console.log(sum(4,5,6));
+
+// Exercício 3
+// Escreva a função personLikes, que recebe como parâmetro os objetos alex ou gunnar. Cada objeto representa uma pessoa, e a função deve retornar todos os gostos daquela pessoa, conforme mostrado abaixo:
+
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+console.log("Exercicio 3");
+
+const alex = {
+    name: 'Alex',
+    age: 26,
+    likes: ['fly fishing'],
+    nationality: 'Australian',
+  };
+  
+  const gunnar = {
+    name: 'Gunnar',
+    age: 30,
+    likes: ['hiking', 'scuba diving', 'taking pictures'],
+    nationality: 'Icelandic',
+  };
+  
+  // complete a assinatura da função abaixo
+  const personLikes = ({name,age,likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+  // <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+  
+  // Retornos esperados:
+  console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+  console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+  // Exercício 4
+// Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+console.log("Exercicio 4");
+
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+    // bornIn: nascido em
+  ];
+  
+  // escreva filterPeople abaixo
+  const filterPeople = () => people.filter( ({nationality,bornIn}) => nationality === 'Australian' && bornIn < 2000 && bornIn > 1899);
+
+  console.log(filterPeople());
+
+
+  // Exercício 5
+  // Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de medida:
+
+  console.log("Exercicio 5");
+  const ships = [
+    {
+      name: 'Titanic',
+      length: 269.1,
+      measurementUnit: 'meters',
+    },
+    {
+      name: 'Queen Mary 2',
+      length: 1132,
+      measurementUnit: 'feet',
+    },
+    {
+      name: 'Yamato',
+      length: 256,
+      measurementUnit: 'meters',
+    },
+    // measurementUnit: unidade de medida
+  ];
+  
+  // escreva shipLength abaixo
+  const shipLength = ({name,length,measurementUnit}) => `${name} is ${length} ${measurementUnit} long`;
+  
+  // retorno esperado
+  console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+  console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+  console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+
+
+  // Exercício 6
+// O objeto yearSeasons representa as estações do ano. Cada chave do objeto é uma estação, e o valor de cada chave é um array de strings que representa os meses daquela estação. A partir desse objeto, desestruture as estações do ano e espalhe-as em um array de meses do ano.
+console.log("Exercicio 6");
+
+const yearSeasons = {
+    spring: ['March', 'April', 'May'],
+    summer: ['June', 'July', 'August'],
+    autumn: ['September', 'October', 'November'],
+    winter: ['December', 'January', 'February'],
+  };
+  // yearSeasons: estações do ano.
+
+  const meses = Object.values(yearSeasons).flat();
+
+  const { spring, summer, autumn, winter } = yearSeasons;
+  const months = [...spring, ...summer, ...autumn, ...winter];
+  console.log(months);
+  console.log(meses);
+  
