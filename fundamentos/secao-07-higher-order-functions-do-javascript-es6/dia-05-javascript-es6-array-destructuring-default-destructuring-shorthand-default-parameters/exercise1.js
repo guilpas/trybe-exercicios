56a87dbc4708c513d716ba893d89c3966a3dfc78
Guilherme P Silva <guilpas@gmail.com>
2023-03-08 16:43:45 -0300

    Exercicio 2

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-shorthand-default-parameters/exercise1.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-shorthand-default-parameters/exercise1.js
@@ -5,3 +5,16 @@ const myList = [5, 2, 3];
 
 // escreva swap abaixo
 const swap = ([firstElement,secondElement,thirdElement]) => [thirdElement,secondElement,firstElement];
+console.log("Ex1",swap(myList));
+
+// Exercício 2
+// Suponha que você esteja trabalhando em projeto de um site de carros, onde cada carro é representado dentro de um array. Então sua liderança pede que seja mudado o formato de array para objeto. Para isso, crie uma função chamada toObject que, dada uma lista, retorna um objeto representando o carro:
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject = ([nome,fabricante,ano]) => ({nome,fabricante,ano});
+console.log("Ex2",toObject(palio));
+console.log("Ex2",toObject(shelbyCobra));
